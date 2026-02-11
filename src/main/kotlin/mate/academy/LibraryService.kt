831e4d93e3cb88c3a130158e86ed96bedaabf91c
Danyil <shelofast.daniil@gmail.com>
2026-02-11 20:15:38 +0200

    homework

--- src/main/kotlin/mate/academy/LibraryService.kt
+++ src/main/kotlin/mate/academy/LibraryService.kt
@@ -1,20 +1,39 @@
 package mate.academy
 
 class LibraryService {
-    fun addBook(book: Book) {
-        // TODO: implement
-    }
+    val books = arrayListOf<Book>()
+
+    fun addBook(book: Book) = books.add(book)
 
     fun searchByTitle(title: String): List<Book> {
-        // TODO: implement
+        val titles = mutableListOf<Book>()
+        for (t in books) {
+            if (t.title.contains(title, ignoreCase = true)) {
+                titles.add(t)
+            }
+        }
+        return titles
     }
 
-
     fun searchByAuthor(authorName: String): List<Book> {
-        // TODO: implement
+        val names = mutableListOf<Book>()
+        for (n in books) {
+            for (name in n.authors) {
+                if (name.name.contains(authorName,ignoreCase = true)) {
+                    names.add(n)
+                }
+            }
+        }
+        return names
     }
 
     fun searchByGenre(genre: String): List<Book> {
-        // TODO: implement
+        val genres = mutableListOf<Book>()
+        for (g in books) {
+            if (g.genre.contains(genre, ignoreCase = true)) {
+                genres.add(g)
+            }
+        }
+        return genres
     }
 }
